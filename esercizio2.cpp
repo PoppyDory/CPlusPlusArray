9efdb95fff0a0ddbb5871c6e8383f24e485950db
PoppyDory <lanza.rosita@gmail.com>
2023-02-27 21:38:46 +0100

    esercizio parola univoca

--- esercizio2.cpp
+++ esercizio2.cpp
@@ -1,7 +1,46 @@
-/*Scrivere un programma in C++ che prende in input una 
-stringa e determina se essa è formata da caratteri univoci, 
+/*Scrivere un programma in C++ che prende in input una
+stringa e determina se essa è formata da caratteri univoci,
 ovvero non ci sono caratteri ripetuti nella stringa.
-Ad esempio, la stringa "casa" non è formata da caratteri 
-univoci poiché la lettera "a" compare due volte, mentre la 
+Ad esempio, la stringa "casa" non è formata da caratteri
+univoci poiché la lettera "a" compare due volte, mentre la
 stringa "cane" è formata da caratteri univoci
 */
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string parola = "";
+    bool noUnivoca = false;
+    // string arrayParole[1];
+
+    // int grandezza= sizeof(arrayParole) / sizeof(arrayParole[0]);
+    cout << "inserisci parola" << endl;
+
+    // for (int i=0; i<grandezza; i++) //scorro l'array
+    //{
+    cin >> parola;
+
+    for (int j = 0; j < parola.length(); j++) // scorrp la parola
+    {
+        for (int k = j + 1; k < parola.length(); k++) // confronto tutte le lettere tra di loro
+        {
+            if (parola[j] == parola[k])
+            {
+                noUnivoca = true;
+            }
+        }
+    }
+    if (noUnivoca)
+    {
+        cout << " la parola non e' univoca. " << endl;
+        
+    }
+    else
+    {
+        cout << "La parola e' univoca" << endl;
+    }
+}
