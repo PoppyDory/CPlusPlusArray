e4e9f3a0c47fb3f737c335bd4e902e8be0199bb0
PoppyDory <lanza.rosita@gmail.com>
2023-02-28 14:34:42 +0100

    esercizio 5 con funzione

--- esercizio5.cpp
+++ esercizio5.cpp
@@ -1,2 +1,39 @@
-//Scrivi una funzione che prenda in input un array di interi e la sua lunghezza e 
-//restituisca true se l'array è ordinato in ordine crescente e false altrimenti.
+// Scrivi una funzione che prenda in input un array di interi e la sua lunghezza e
+// restituisca true se l'array è ordinato in ordine crescente e false altrimenti.
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+bool arrayOrdinato(int array[], int dimensione)
+{
+    bool ordinato=false;
+    for (int i = 0; i < dimensione; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            return false;
+            break;
+        }
+        else 
+        {
+            return true;
+        }
+    }
+    return ordinato;
+}
+
+int main()
+{
+    int array[4] = {1, 2, 3, 6};
+    int dimensione = sizeof(array) / sizeof(array[0]);
+
+    if (arrayOrdinato(array, dimensione)==0)
+    {
+        cout<< "l'array non e' ordinato ";
+    }
+    else
+    {
+        cout<< " l'array e' ordinato ";
+    }
+}
