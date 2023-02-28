37940495bfd65986b3fc8c87d0f6b8cb00c5b4dd
PoppyDory <lanza.rosita@gmail.com>
2023-02-28 12:37:54 +0100

    esercizio 4 main

--- esercizio4.cpp
+++ esercizio4.cpp
@@ -7,5 +7,17 @@ using namespace std;
 
 int main()
 {
+    int array[4]= {4,2,3,6};
+    int dimensione = sizeof(array) / sizeof(array[0]);
+    int maggiore=0;
     
+    for(int i=0; i<dimensione; i++)
+    {
+        if (array[i]<array[i+1])
+        {
+            maggiore=array[i+1];
+        }
+
+    }
+    cout<< maggiore;
 }
