7659fb8e9bc020e372a5cf746f264e53c41dc118
PoppyDory <lanza.rosita@gmail.com>
2023-03-06 10:40:03 +0100

    es 12 corretto, stampa la posizione

--- esercizio12.cpp
+++ esercizio12.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int* arrayContaUguali(int array1[], int array2[], int dimensione1, int dimensione2)
 {
     int uguali = 0;
-    int arrayComuni[uguali];
 
     for (int i = 0; i < dimensione1; i++)
     {
@@ -14,9 +13,27 @@ int* arrayContaUguali(int array1[], int array2[], int dimensione1, int dimension
             if (array1[i] == array2[j])
             {
                 uguali++;
-                arrayComuni[i]=array1[i];
             }
         }
+    }
+    
+    int arrayComuni[uguali];
+    for (int i = 0; i < uguali; i++)
+    {
+        for (int j = 0; j < dimensione1; j++)
+        {
+            for (int k = 0; k < dimensione2; k++)
+            {
+                if (array1[j] == array2[k])
+                {
+                    arrayComuni[i] = array1[j];
+                }
+            }
+        }
+    }    
+    for (int i = 0; i < uguali; i++)
+    {
+        cout << arrayComuni << endl;
     }
     return arrayComuni;
 }
@@ -25,10 +42,11 @@ int main()
 {
     int array1[4] = {1, 2, 3, 4};
     int array2[4] = {2, 4, 6, 8};
-    
-
     int dimensione1 = sizeof(array1) / sizeof(array1[0]);
     int dimensione2 = sizeof(array2) / sizeof(array2[0]);
 
-    cout << arrayContaUguali(array1, array2, dimensione1, dimensione2);
+    int* arrayComuni = arrayContaUguali(array1, array2, dimensione1, dimensione2);
+
+
+    cout<<"I numeri in comune sono: "<< arrayComuni; 
 }
